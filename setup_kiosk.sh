8dda26cdaf6407aae753eb7927e5ddb9607659b5
pmoriconi <77704949+pmoriconi@users.noreply.github.com>
2024-10-04 01:24:24 -0300

    Update setup_kiosk.sh

--- setup_kiosk.sh
+++ setup_kiosk.sh
@@ -4,24 +4,27 @@
 sudo apt-get update
 sudo apt-get install -y unclutter matchbox-window-manager
 
-# 2) Copiar ejecutable y archivo de configuración 
-cp kiosk-bin /home/selec/kiosk-bin
-cp kiosk.conf /home/selec/kiosk.conf
+# 2) Generar archivo de configuración kiosk.conf con la URL
+KIOSK_CONF_PATH="/home/selec/kiosk.conf"
+echo 'URL="http://192.168.100.115:88/billboard/6"' > $KIOSK_CONF_PATH
 
-# 3) Editar .bashrc del home de usuario selec
+# 3) Copiar ejecutable
+cp kiosk-bin /home/selec/kiosk-bin
+
+# 4) Editar .bashrc del home de usuario selec
 BASHRC_PATH="/home/selec/.bashrc"
 if ! grep -q "xinit /home/selec/kiosk-bin -- vt\$(fgconsole)" $BASHRC_PATH; then
   echo "xinit /home/selec/kiosk-bin -- vt\$(fgconsole) >/dev/null 2>&1" >> $BASHRC_PATH
 fi
 
-# 4) Editar el archivo /boot/firmware/cmdline.txt
+# 5) Editar el archivo /boot/firmware/cmdline.txt
 CMDLINE_PATH="/boot/firmware/cmdline.txt"
 CMDLINE_CHANGES="console=serial0,115200 console=tty3 root=PARTUUID=212ade2c-02 rootfstype=ext4 fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles cfg80211.ieee80211_regdom=AR logo.nologo vt.global_cursor_default=0"
 if ! grep -q "console=serial0,115200 console=tty3" $CMDLINE_PATH; then
   sudo sed -i "1s|^|$CMDLINE_CHANGES |" $CMDLINE_PATH
 fi
 
-# 5) Editar el archivo /boot/firmware/config.txt
+# 6) Editar el archivo /boot/firmware/config.txt
 CONFIG_PATH="/boot/firmware/config.txt"
 if ! grep -q "display_hdmi_rotate=0" $CONFIG_PATH; then
   echo -e "\n# Rotate display (for portrait displays you must disable DRM VC4 V3D and max_fr>\n# 0 = 0°, 1 = 90°, 2 = 180°, 3 = 270°\ndisplay_hdmi_rotate=0" | sudo tee -a $CONFIG_PATH > /dev/null
@@ -33,7 +36,7 @@ fi
 
 cp /home/selec/rpi-tv/splash.png /home/selec/splash.png
 
-# 6) Reemplazar el archivo /usr/share/plymouth/themes/pix/splash.png con /home/selec/splash.png
+# 7) Reemplazar el archivo /usr/share/plymouth/themes/pix/splash.png con /home/selec/splash.png
 PLYMOUTH_SPLASH_PATH="/usr/share/plymouth/themes/pix/splash.png"
 if [ -f "/home/selec/splash.png" ]; then
   sudo cp /home/selec/splash.png $PLYMOUTH_SPLASH_PATH
@@ -42,7 +45,27 @@ else
   echo "El archivo /home/selec/splash.png no existe, no se pudo reemplazar splash.png"
 fi
 
-# 7) Actualizar initramfs y reiniciar
+# 8) Crear un script para verificar y eliminar la carpeta /home/selec/rpi-tv
+INIT_SCRIPT_PATH="/etc/init.d/check_rpi_tv"
+sudo bash -c "cat > $INIT_SCRIPT_PATH" << 'EOF'
+#!/bin/bash
+
+# Verificar y eliminar la carpeta /home/selec/rpi-tv si existe
+if [ -d /home/selec/rpi-tv ]; then
+  rm -rf /home/selec/rpi-tv
+  echo "La carpeta /home/selec/rpi-tv ha sido eliminada."
+else
+  echo "La carpeta /home/selec/rpi-tv no existe."
+fi
+EOF
+
+# 9) Hacer el script ejecutable
+sudo chmod +x $INIT_SCRIPT_PATH
+
+# 10) Agregar el script al inicio del sistema
+sudo update-rc.d check_rpi_tv defaults
+
+# 11) Actualizar initramfs y reiniciar
 sudo truncate -s 0 /etc/issue
 sudo truncate -s 0 /etc/issue.net
 sudo truncate -s 0 /etc/motd
